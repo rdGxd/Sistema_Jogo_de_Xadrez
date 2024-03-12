9c5622cc3c71995ff55bca673ef168361a46b195
Rodrigo Silva <rdg.hora1@gmail.com>
2024-03-12 12:06:08 -0300

    Starting to implement Board and Piece

--- src/application/App.java
+++ src/application/App.java
@@ -2,14 +2,14 @@ package application;
 
 import java.util.Scanner;
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class App {
   public static void main(String[] args) throws Exception {
     Scanner sc = new Scanner(System.in);
 
-    Position pos = new Position(3, 5);
-    System.out.println(pos);
+    Board board = new Board(8, 8);
+
     sc.close();
   }
 }
